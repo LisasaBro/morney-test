9b394bd371f447eb7ef0b07330c579102328a604
sasaLi <850852006@qq.com>
2021-12-16 20:27:33 +0800

    创建Nav

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <span>您所访问的路径不存在！！！</span>
+  <router-link to="/">返回首页</router-link>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistcs.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  Statistcs Vue
+  <Nav/>
+</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Statistcs'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
