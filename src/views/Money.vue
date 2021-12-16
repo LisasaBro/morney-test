6de73a687b9eea9db150cd665fca5b811b1a9c84
sasaLi <850852006@qq.com>
2021-12-16 20:38:02 +0800

    页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,9 @@
 <template>
-<div>Money Vue
-<Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content"> Money Vue</div>
+
+    <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,5 +13,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.nav-wrapper {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  overflow: auto;
+  flex-grow:1;
+}
 </style>
