5e918a1c967e519947e0df97f94798b557eeeade
sasaLi <850852006@qq.com>
2021-12-25 16:41:22 +0800

    完成 Types 组件

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
+  <div class="layout-wrapper" :class="classPrefix && `${classPrefix}-wrapper`">
+    <div class="content" :class="classPrefix && `${classPrefix}-content`   ">
       <slot/>
     </div>
-
     <Nav/>
   </div>
 </template>
 
 <script lang="ts">
 export default {
+  props: ['classPrefix'],
   name: 'Layout'
 };
 </script>
 
 <style lang="scss" scoped>
-.nav-wrapper {
-  border: 1px solid red;
+.layout-wrapper {
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
-
 .content {
   overflow: auto;
   flex-grow: 1;
