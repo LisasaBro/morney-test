7a33bc8cce7981a7da796a6b454b0b5de11db189
sasaLi <850852006@qq.com>
2021-12-29 15:17:57 +0800

    添加标签可跳转编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,12 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id"><span>{{ tag.name }}</span>
+    <div class="tags">
+      <router-link class="tag"
+                   v-for="tag in tags" :key="tag.id"
+                   :to="`/labels/edit/${tag.id}`"><span>{{ tag.name }}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新增标签</button>
     </div>
@@ -24,9 +26,9 @@ export default class Labels extends Vue {
   createTag() {
     const name = window.prompt('请输入标签名');
     if (name) {
-      const message = tagListModel.create(name)
-      if(message === 'duplicated'){
-        window.alert('标签名重复')
+      const message = tagListModel.create(name);
+      if (message === 'duplicated') {
+        window.alert('标签名重复');
       }
     }
   }
@@ -38,7 +40,7 @@ export default class Labels extends Vue {
   font-size: 16px;
   padding-left: 16px;
 
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
