474df0e4c960d3643d66f10ac2c1a754ae519fd6
sasaLi <850852006@qq.com>
2021-12-30 14:00:04 +0800

    重新封装 recordListModel

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -2,7 +2,9 @@ let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
 
 function createId() {
   id++;
+  //讲id存到localStorage
+  window.localStorage.setItem('_idMax', id.toString());
   return id;
 }
 
-export default createId
+export default createId;
