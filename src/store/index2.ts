45169bda7b5577e780b062f768996e012597057c
sasaLi <850852006@qq.com>
2022-01-01 17:02:35 +0800

    模块化了 store 删除了models

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,35 +1,11 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  // record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record:RecordItem) => recordListModel.create(record),
-// tag store
-  tagList : tagListModel.fetch(),
-  createTag : (name: string) => {
-    const message = tagListModel.create(name);
-    if (message === 'duplicated') {
-      window.alert('标签名重复');
-    }
-  },
+ ...recordStore,
+  ...tagStore
 
-  removeTag : (id: string) => {
-    if (tagListModel.remove(id)) {
-      return true;
-    } else {
-      return false;
-    }
-  },
-
-  updateTag : (id: string, name: string) => {
-    return tagListModel.update(id, name);
-  },
-
-  findTag  (id: string)  {
-    return this.tagList.filter(t => t.id === id)[0];
-
-  }
 }
 
 export default store;
