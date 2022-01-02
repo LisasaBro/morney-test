0436743ad0a513c678d4d270ced199f371b2abe3
sasaLi <850852006@qq.com>
2022-01-02 17:29:03 +0800

    所有computed 改成 get 函数

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -14,7 +14,6 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
 import {mixins} from 'vue-class-component';
@@ -23,13 +22,11 @@ import TagHelper from '@/mixins/TagHelper';
 
 @Component({
   components: {Button},
-  computed: {
-    tags() {
-      return this.$store.state.tagList;
-    }
-  }
 })
 export default class Labels extends mixins(TagHelper){
+  get tags() {
+    return this.$store.state.tagList;
+  }
   beforeCreated() {
     this.$store.commit('fetchTag');
   }
