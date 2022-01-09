d2f5d2f39ad7f39f2184962b80a829f287e35292
sasaLi <850852006@qq.com>
2022-01-09 16:28:47 +0800

    完成列表的CSS

--- src/views/Statistcs.vue
+++ src/views/Statistcs.vue
@@ -2,8 +2,64 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
+    <ol>
+      <li v-for="(group,index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li class="record" v-for="item in group.items" :key="item.id">
+            <span>{{ tagString(item.tags) }}</span>
+
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constant/intervalList';
+import recordTypeList from '@/constant/recordTypeList';
+
+@Component({
+  components: {Tabs}
+})
+export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    type HashTableValue = { title: string, items: RecordItem[] }
+    const hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecord');
+  }
+
+  type = '-';
+  interval = 'day';
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
+}
+</script>
+
 <style scoped lang="scss">
 ::v-deep .type-tabs-item {
   background: white;
@@ -17,26 +73,31 @@
   }
 
 }
-::v-deep .interval-tabs-item{
+
+::v-deep .interval-tabs-item {
   height: 48px;
 }
-</style>
 
-<script lang="ts">
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import Tabs from '@/components/Tabs.vue';
-import intervalList from '@/constant/intervalList';
-import recordTypeList from '@/constant/recordTypeList';
-
-@Component({
-  components: {Tabs}
-})
-export default class Statistics extends Vue {
-  type = '-';
-  interval = 'day';
-  intervalList = intervalList;
-  recordTypeList = recordTypeList;
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
 }
-</script>
 
+.title {
+  @extend %item;
+}
+
+.record {
+  background: white;
+  @extend %item;
+}
+
+.notes {
+  margin-right: auto;
+  margin-left: 8px;
+  color: #999999;
+}
+</style>
